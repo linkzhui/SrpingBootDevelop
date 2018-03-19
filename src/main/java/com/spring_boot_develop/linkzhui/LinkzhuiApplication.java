6cae9969bb4dbdeb15b5c6e9750ac164b914efc7
Zhemin_Su <Raymondsu2010@gmail.com>
2018-03-19 16:13:05 -0700

    set the debug mode to true

--- src/main/java/com/spring_boot_develop/linkzhui/LinkzhuiApplication.java
+++ src/main/java/com/spring_boot_develop/linkzhui/LinkzhuiApplication.java
@@ -1,5 +1,7 @@
 package com.spring_boot_develop.linkzhui;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,11 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class LinkzhuiApplication {
 
+	@Autowired
+	private AuthorSettings authorSettings;
+
 	@RequestMapping("/")
 	String index(){
-		return "hello world";
+		return "book name is: "+authorSettings.getName()+" and book author is: "+authorSettings.getAge();
 	}
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(LinkzhuiApplication.class, args);
 	}
 }
